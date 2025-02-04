fa449847ef0c061963e24cff159db26639968f7c
Charles Fernandes de Souza <souzach@outlook.com>
2025-02-04 19:06:51 -0300

    FoodRequestDTOimplements Serializable

--- Backend/digitalMenu/src/main/java/com/souza/charles/digitalMenu/dto/FoodRequestDTO.java
+++ Backend/digitalMenu/src/main/java/com/souza/charles/digitalMenu/dto/FoodRequestDTO.java
@@ -5,5 +5,7 @@ package com.souza.charles.digitalMenu.dto;
  Project adapted by: Charles Fernandes de Souza
  Date: January 31, 2025
  */
-public record FoodRequestDTO(String title, Double price, String image) {
+import java.io.Serializable;
+
+public record FoodRequestDTO(String title, Double price, String image) implements Serializable{
 }
